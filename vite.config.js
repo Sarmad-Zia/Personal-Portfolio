042397199acbb00a68971fb2fe620ec5b92188d7
Sarmad Zia <sarmadzia565@gmail.com>
2025-06-26 23:58:30 +0500

    initial

--- vite.config.js
+++ vite.config.js
@@ -13,14 +13,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 // Get the repository name from environment variables or directly define it
 // Make sure this matches your GitHub repository name exactly!
-const repoName = "Personal-Portfolio"; // <<< REPLACE WITH YOUR ACTUAL REPO NAME
+// const repoName = "Personal-Portfolio"; // <<< REPLACE WITH YOUR ACTUAL REPO NAME
 
 export default defineConfig(({ command }) => {
-  const isProd = command === 'build'; // Check if it's a build command
+  // const isProd = command === 'build'; // Check if it's a build command
 
   return {
     plugins: [react(),tailwindcss()],
     // Set base path dynamically
-    base: isProd ? `/${repoName}/` : '/', 
+    // base: isProd ? `/${repoName}/` : '/', 
   };
 });
